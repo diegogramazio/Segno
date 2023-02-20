d51722a4f2ce7aeb16c097d76273c1907d6d4f9f
diegogramazio <116880984+diegogramazio@users.noreply.github.com>
2023-02-20 10:43:29 +0100

    Update Segno.c

--- Segno.c
+++ Segno.c
@@ -1,1 +1,14 @@
 
+		/* INPUT */
+		printf("Caro utente, introduci un intero\n");
+		scanf("%d", &intero);
+		
+		/* OUTPUT */
+		if(intero>0)
+			    printf("POSITIVO\n");
+		else
+			    if(intero==0)
+					     printf("NULLO\n");
+				else
+					     printf("NEGATIVO\n");
+}
